f2c909f9ee014ee23547f575d30ff9a2ac7fbed0
EP <kjuioqq8@gmail.com>
2022-08-13 22:33:08 +0900

    refactor: 중복 코드 제거

--- src/test/kotlin/BallTest.kt
+++ src/test/kotlin/BallTest.kt
@@ -1,10 +1,18 @@
 import domain.Ball
 import domain.MatchStatus
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class BallTest {
+
+    private lateinit var ball: Ball
+
+    @BeforeEach
+    internal fun setUp() {
+        ball = Ball(position = 1, ballNumber = 3)
+    }
 
     @Test
     fun `볼은 1~9 이외의 숫자를 입력했을 때 예외가 발생한다`() {
@@ -15,21 +23,18 @@ class BallTest {
 
     @Test
     fun `볼의 숫자가 다르면 Nothing 이다`() {
-        val ball = Ball(position = 1, ballNumber = 3)
         val targetBall = Ball(position = 1, ballNumber = 4)
         assertThat(ball.match(targetBall)).isEqualTo(MatchStatus.NOTHING)
     }
 
     @Test
     fun `볼의 위치는 다르지만 숫자가 같으면 ball 이다`() {
-        val ball = Ball(position = 1, ballNumber = 3)
         val targetBall = Ball(position = 2, ballNumber = 3)
         assertThat(ball.match(targetBall)).isEqualTo(MatchStatus.BALL)
     }
 
     @Test
     fun `볼의 위치와 숫자가 같으면 strike 이다`() {
-        val ball = Ball(position = 1, ballNumber = 3)
         val targetBall = Ball(position = 1, ballNumber = 3)
         assertThat(ball.match(targetBall)).isEqualTo(MatchStatus.STRIKE)
     }
